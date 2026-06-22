3c7c06c2ed638143ce81ed64179bb93cdc076b9f
MimoCode <mimocode@users.noreply.github.com>
2026-06-22 23:03:29 +0300

    fix: replace no-op test with real assertions, disable public IP for Fargate tasks
    - test: replace commented-out SQS test with actual ECS stack assertions (cluster, service, ALB, VPC, autoscaling)
    - fix: set assignPublicIp to false since VPC already has NAT gateways for outbound traffic

--- test/ecs-cdk.test.ts
+++ test/ecs-cdk.test.ts
@@ -1,17 +1,52 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as EcsCdk from '../lib/ecs-cdk-stack';
+import * as cdk from 'aws-cdk-lib/core';
+import { Template } from 'aws-cdk-lib/assertions';
+import { EcsCdkStack } from '../lib/ecs-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/ecs-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new EcsCdk.EcsCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+let app: cdk.App;
+let stack: EcsCdkStack;
+let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+beforeEach(() => {
+  app = new cdk.App();
+  stack = new EcsCdkStack(app, 'TestStack', {
+    env: { account: '123456789012', region: 'eu-central-1' },
+    envName: 'test',
+  });
+  template = Template.fromStack(stack);
+});
+
+test('creates ECS cluster with Container Insights', () => {
+  template.hasResourceProperties('AWS::ECS::Cluster', {
+    ClusterSettings: [
+      {
+        Name: 'containerInsights',
+        Value: 'enabled',
+      },
+    ],
+  });
+});
+
+test('creates Fargate service with desired count', () => {
+  template.hasResourceProperties('AWS::ECS::Service', {
+    DesiredCount: 2,
+    LaunchType: 'FARGATE',
+  });
+});
+
+test('creates Application Load Balancer', () => {
+  template.hasResourceProperties('AWS::ElasticLoadBalancingV2::LoadBalancer', {
+    Scheme: 'internet-facing',
+    Type: 'application',
+  });
+});
+
+test('creates VPC with subnets', () => {
+  template.resourceCountIs('AWS::EC2::VPC', 1);
+});
+
+test('creates auto scaling target', () => {
+  template.hasResourceProperties('AWS::ApplicationAutoScaling::ScalableTarget', {
+    MinCapacity: 1,
+    MaxCapacity: 10,
+  });
 });
